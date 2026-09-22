5fa1a3831fb55800df5717cf6047f64bee52f9f8
Ho Cong Truc <hctruc357@gmail.com>
2026-09-22 13:45:53 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import streamlit as st
 # Cấu hình giao diện Streamlit hiển thị tối ưu trên cả điện thoại và máy tính
 st.set_page_config(page_title="SFE/SWE Thermophysical Tool", layout="centered")
 
-st.title("🔬 Công Cụ Nhiệt Động Lực Học Dung Môi Tới Hạn & Siêu Áp")
+st.title("🔬 Công Cụ Nhiệt Động Lực Học Dung Môi Siêu Tới Hạn & Chất lỏng áp suất")
 st.caption(
-    "Phát triển bởi TS. Chuyên gia Công nghệ Thực phẩm & Thiết bị Hóa học"
+    "Phát triển bởi TS. Hồ Công "
 )
 
 # =========================================================================
